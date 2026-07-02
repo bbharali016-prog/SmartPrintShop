bfa004821f12647367c101218ad39f213ad0154c
bitupanbharali69-del <bitupanbharali69@gmail.com>
2026-07-02 16:46:47 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import subprocess
+
+from flask import Flask, render_template, request , redirect, flash
+import os
+
+app = Flask(__name__)
+app.secret_key = "smartprintshop123"
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Upload folder create if not exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Temporary memory storage
+jobs = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload-page")
+def upload_page():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    customer_name = request.form.get("name")
+    print_type = request.form.get("print_type")
+    copies = request.form.get("copies")
+    paper_size = request.form.get("paper_size")
+
+    file = request.files["file"]
+
+    filename = file.filename
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+    jobs.append({
+        "id": len(jobs)+1,
+        "customer_name": customer_name,
+        "filename": filename,
+        "print_type": print_type,
+        "copies": copies,
+        "paper_size": paper_size,
+        "status":"Pending"
+    })
+
+    flash("✅ File uploaded successfully!")
+    return redirect("/upload-page")
+
+@app.route("/cancel/<int:id>")
+def cancel_job(id):
+    for job in jobs:
+        if job["id"] == id:
+            job["status"] = "Failed"
+
+    return redirect("/admin")
+
+@app.route("/print/<int:id>")
+def print_job(id):
+    for job in jobs:
+        if job["id"] == id:
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], job["filename"])
+
+            # File ko default printer par bheje
+            subprocess.run(["lp", filepath])
+
+            job["status"] = "Completed"
+
+    return redirect("/admin")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", jobs=jobs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
